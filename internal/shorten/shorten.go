23a7f3861b0f2eb15b52074d7a8f3928249c7633
iNgredie <sandslash@bk.ru>
2023-02-10 00:24:52 +0400

    first commit

---
+++ internal/shorten/shorten.go
@@ -0,0 +1,32 @@
+package shorten
+
+import "strings"
+
+const aplphabet = "ynAJfoSgdXHB5VasEMtcbPCr1uNZ4LG723ehWkvwYR6KpxjTm8iQUF"
+
+var aplphabetLen = uint32(len(aplphabet))
+
+func Shorten(id uint32) string {
+	var (
+		nums    []uint32
+		num     = id
+		builder strings.Builder
+	)
+	for num > 0 {
+		nums = append(nums, num%aplphabetLen)
+		num /= aplphabetLen
+	}
+
+	reverse(nums)
+
+	for _, num := range nums {
+		builder.WriteString(string(aplphabet[num]))
+	}
+	return builder.String()
+}
+
+func reverse(s []uint32) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
